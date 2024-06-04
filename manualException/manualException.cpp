148c9b66f4780cd2e2f67fddfef1f0de41212864
cahyasaputra09 <`Lenovo@DESKTOP-Q4UO40F>
2024-06-04 19:04:12 +0700

    selesai dalam menulis

---
+++ manualException/manualException.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+
+    }
+    catch (int a) {
+        //blok ini akaan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+
+    }
+    catch (...) {
+        /*Jika selain integer maka blok ini akan dieksekusi*/
+        cout << "default pengecualian diexsekusi " << endl;
+
+    }
+    return 0;
+}
