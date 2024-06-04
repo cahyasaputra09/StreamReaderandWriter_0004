7e71f1bbdac5ed9cd2d00b92346f3ae655a28235
cahyasaputra09 <`Lenovo@DESKTOP-Q4UO40F>
2024-06-04 17:45:05 +0700

    membuka file dalam mode menulis

---
+++ ioFilewithpath/ioFilewithpath.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan nama file:";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">=Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+
+    while (true) {
