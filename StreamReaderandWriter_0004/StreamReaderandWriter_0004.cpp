3142a1b2ff2927a691cbeaaa8d7d7ebe6b3a58c4
cahyasaputra09 <`Lenovo@DESKTOP-Q4UO40F>
2024-06-04 17:24:08 +0700

    int main

--- StreamReaderandWriter_0004/StreamReaderandWriter_0004.cpp
+++ StreamReaderandWriter_0004/StreamReaderandWriter_0004.cpp
@@ -3,3 +3,18 @@
 #include <string>
 using namespace std;
 
+int main() {
+	string baris;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjukan ke sebuah nama file 
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+
+	}
+}
