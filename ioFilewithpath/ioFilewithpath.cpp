f1a3eea1716c7c2cced58db87d434b827954a45e
cahyasaputra09 <`Lenovo@DESKTOP-Q4UO40F>
2024-06-04 18:19:08 +0700

    selesai dalam md=ode membaca

--- ioFilewithpath/ioFilewithpath.cpp
+++ ioFilewithpath/ioFilewithpath.cpp
@@ -21,3 +21,35 @@ int main() {
     //unlimited loop untuk menulis
 
     while (true) {
+        while (true) {
+            cout << "- ";
+            //mendapatkan setiap karakter dalam satu kelas
+            getline(cin, baris);
+            //loop akan berhenti jika anda memasukan karakter q
+            if (baris == "q")break;
+            //menulis dan memasukan nilai dari 'baris' kedalam file
+            outfile << baris << endl;
+        }
+        //selesai dalam menulis sekarang tutup filenya
+        outfile.close();
+
+        //membuka file dalam mode membaca
+        ifstream infile;
+        //menunjuk ke sebuah file
+        infile.open(NamaFile + ".txt", ios::in);
+
+        cout << endl << ">= membuka dan membaca file  " << endl;
+        //jika file ada maka
+
+        if (infile.is_open())
+        {
+
+            //melakukan perulanngan setiap baris
+            while (getline(infile, baris))
+            {
+                //dan tampilkan disini
+                cout << baris << '\n';
+            }
+            //tutup file tersebut setelah selesai
+            
+
